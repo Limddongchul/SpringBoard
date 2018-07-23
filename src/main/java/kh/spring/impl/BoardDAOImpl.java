71d3da318fa1081f3ddd966849c771e4b15bdcdd
임동철 <star379812@gmail.com>
2018-07-23 18:37:21 +0900

    페이지네비 적용

--- src/main/java/kh/spring/impl/BoardDAOImpl.java
+++ src/main/java/kh/spring/impl/BoardDAOImpl.java
@@ -1,12 +1,11 @@
 package kh.spring.impl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import kh.spring.dto.BoardDTO;
@@ -16,9 +15,110 @@ import kh.spring.interfaces.BoardDAO;
 public class BoardDAOImpl implements BoardDAO {
 
 	@Autowired
-	private JdbcTemplate template;
+//	private JdbcTemplate template;
+	private SqlSessionTemplate template;
 
 	@Override
+	public int insert(BoardDTO dto) {
+		return template.insert("Board.insert",dto);
+	}
+
+	@Override
+	public List<BoardDTO> getList(int startNum, int endNum) {
+		Map<String,Integer> map = new HashMap<>();
+		map.put("startNum", startNum);
+		map.put("endNum",endNum);
+		return template.selectList("Board.getList",map);
+	}
+	
+	@Override
+	public String getPageNavi(int currentPage) {
+		
+		int recordTotalCount=0;
+
+		recordTotalCount = template.selectOne("Board.getPageNavi");
+		
+		int recordCountPerPage = 10; 
+		int naviCountPerPage = 10;
+		int pageTotalCount = 0; 
+
+		if(recordTotalCount % recordCountPerPage > 0) { 
+			pageTotalCount = recordTotalCount / recordCountPerPage + 1;
+		}else {
+			pageTotalCount = recordTotalCount / recordCountPerPage;
+		}
+
+
+
+		if(currentPage < 1) {
+			currentPage = 1;
+		}else if(currentPage > pageTotalCount) {
+			currentPage = pageTotalCount;
+		}
+
+		int startNavi = (currentPage - 1)/ naviCountPerPage * naviCountPerPage + 1;
+		int endNavi = startNavi + (naviCountPerPage - 1);
+
+		if(endNavi > pageTotalCount) {
+			endNavi = pageTotalCount;
+		}
+
+
+		boolean needPrev = true;
+		boolean needNext = true;
+
+		if(startNavi == 1) {
+			needPrev = false;
+		}
+
+		if(endNavi == pageTotalCount) {
+			needNext = false;
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		if(needPrev) {
+			sb.append("<a href='boardView.do?currentPage="+(startNavi-1)+"'< </a>");
+		}
+		for(int i = startNavi;i <= endNavi;i++) {
+			if(currentPage == i) {
+				sb.append("<a href='boardView.do?currentPage=" + i + "'> <b>" + i + "</b></a>");
+			}else {
+				sb.append("<a href='boardView.do?currentPage=" + i + "'> " + i + "</a>");
+			}
+		}
+		if(needNext) {
+			sb.append("<a href='boardView.do?currentPage="+(endNavi+1)+"'>></a>");
+		}
+
+
+
+		String result = sb.toString();
+
+		return result;
+	}
+
+	@Override
+	public BoardDTO BoardView(int seq) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(int seq) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(BoardDTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+
+	/*@Override
 	public int insert(BoardDTO dto) {
 		String sql = "insert into spring_board values(spring_board_seq.nextval, ?, ?, ?, 10)";
 		return template.update(sql, dto.getTitle(),dto.getContents(), dto.getWriter());
@@ -147,6 +247,6 @@ public class BoardDAOImpl implements BoardDAO {
 		String result = sb.toString();
 
 		return result;
-	}
+	}*/
 
 }
